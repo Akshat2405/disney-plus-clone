409e61a3bf3da97e58d3b441554c095f77c18401
Akshat <akshat87001@gmail.com>
2021-05-14 17:01:23 +0530

    Adding video on background

--- src/componenets/Viewers.js
+++ src/componenets/Viewers.js
@@ -5,39 +5,38 @@ function Viewers() {
     return (
         <Container>
             <Wrap>
-                <img src="/images/viewers-disney.png"/>
-                {/* <div><video width="100%" height="100%" loop >
+                <img src="/images/viewers-disney.png"/> 
+                <video   loop  muted>
                 <source src="/videos/1564674844-disney.mp4" type="video/mp4"/>
-                Your browser does not support the video tag.
-                </video></div> */}
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-pixar.png"/>
-                {/* <div><video width="100%" height="100%" loop >
+                <video   loop  muted >
                 <source src="/videos/1564676714-pixar.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
-                </video></div> */}
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-marvel.png"/>
-                {/* <div><video width="100%" height="100%" autoplay loop >
-                <source src="/videos/1564676115-marvel.mp4"/>
+                <video loop  muted >
+                <source src="/videos/1564676115-marvel.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
-                </video></div> */}
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-starwars.png"/>
-                {/* <div><video width="100%" height="100%" loop >
-                <source src="/videos/1608229455-star-wars.mp4"/>
+                <video   loop  muted >
+                <source src="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
-                </video></div> */}
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-national.png"/>
-                {/* <div><video width="100%" height="100%" loop >
+                <video   loop  muted >
                 <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
-                </video></div> */}
+                </video>
             </Wrap>
         </Container>
     )
@@ -59,7 +58,7 @@ const Wrap=styled.div`
     box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px, rgb(0 0 0 /73%) 0px 16px 10px -10px;
     transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
     img{
-        // position: relative;
+        position: relative;
             width: 100%;
             height: 100%;
             object-fit: cover;
@@ -69,13 +68,21 @@ const Wrap=styled.div`
         transform: scale(1.05);
         border-color: rgba(249, 249, 249, 0.8);
     }
-    // div {
-    //     position: absolute;
-    //     top: 0;
-    //     left: 0;
-    //     object-fit: cover;
-    //     width: 100%;
-    //     height: 100%;
-    // }
+    &: hover{
+        img{
+            z-index:-2;
+        }
+    }
+    video{
+        position: absolute;
+        width: 255px;
+        height: 100%;
+        top: 0;
+        bottom: 0;
+        left: 0%;
+        right: 0%;
+        z-index: -1;
+    }
+    
     
 `
